223e4987ecf422eaf7f3bad87a0291639a61b8ec
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-01-06 23:57:20 -0600

    💄 ui completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import CalculatorForm from './components/CalculatorForm'
 
 export default function App() {
 	return (
 		<div className="app">
-			<h1>React + Sass template</h1>
+			<CalculatorForm />
 		</div>
 	)
 }
