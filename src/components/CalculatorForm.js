c119c63893e156ae9727eef80491bd2ca5a11a57
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-01-08 11:45:00 -0600

    ✨ use form replaced by formik

--- src/components/CalculatorForm.js
+++ src/components/CalculatorForm.js
@@ -1,45 +1,72 @@
 import React from 'react'
-import useForm from '../hooks/useForm'
+import * as yup from 'yup'
+import { useFormik } from 'formik'
 
 import Button from './Button'
 import TextInput from './TextInput'
 
 export default function CalculatorForm() {
-	const { fields, onChange, submit } = useForm({
-		onSubmit: (data) => console.log(data),
+	const { values, errors, handleChange, handleSubmit } = useFormik({
 		initialValues: {
 			Deposito: '',
 			Contribucion: '',
 			Anios: '',
 			Interes: '',
 		},
+		onSubmit: (values) => {
+			console.log(values)
+		},
+		validationSchema: yup.object({
+			Deposito: yup
+				.number()
+				.required('Campo requerido.')
+				.typeError('Solo números.'),
+			Contribucion: yup
+				.number()
+				.required('Campo requerido.')
+				.typeError('Solo números.'),
+			Anios: yup
+				.number()
+				.required('Campo requerido.')
+				.typeError('Solo números.'),
+			Interes: yup
+				.number()
+				.required('Campo requerido.')
+				.typeError('Solo números.')
+				.min(0, 'El número mínimo es uno.')
+				.max(1, 'El número máximo es uno.'),
+		}),
 	})
 	return (
 		<div className="calculator-form">
-			<form className="calculator-form__form" onSubmit={submit}>
+			<form className="calculator-form__form" onSubmit={handleSubmit}>
 				<TextInput
 					name="Deposito"
 					label="Deposito inicial:"
-					value={fields.Deposito}
-					onChange={onChange}
+					value={values.Deposito}
+					onChange={handleChange}
+					error={errors.Deposito}
 				/>
 				<TextInput
 					name="Contribucion"
 					label="Contribución mensual:"
-					value={fields.Contribucion}
-					onChange={onChange}
+					value={values.Contribucion}
+					onChange={handleChange}
+					error={errors.Contribucion}
 				/>
 				<TextInput
 					name="Anios"
 					label="Años:"
-					value={fields.Anios}
-					onChange={onChange}
+					value={values.Anios}
+					onChange={handleChange}
+					error={errors.Anios}
 				/>
 				<TextInput
 					name="Interes"
 					label="Interés estimado:"
-					value={fields.Interes}
-					onChange={onChange}
+					value={values.Interes}
+					onChange={handleChange}
+					error={errors.Interes}
 				/>
 				<Button>Calcular</Button>
 			</form>
